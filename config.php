cd891798abf503517baa76b7ff7796a96797818e
Yasser Rabi <yachironi@hotmail.com>
2013-09-26 09:25:12 +0100

    clearing code structure

--- config.php
+++ config.php
@@ -1,12 +1,11 @@
-<?php 
+<?php
 
-$db="DB Name";   
-$connection = mysql_connect("host","user","password") or die ("Database Connection Problem"); 
-// test la connection 
-if ( ! $connection ) 
-  die ("connection impossible"); 
-  // Connecte la base 
-  mysql_select_db($db) or die ("pas de connection"); 
-
-?> 
+$db = "DB Name";
+$connection = mysql_connect("host", "user", "password") or die("Database Connection Problem");
+// Test Connection
+if (!$connection)
+	die("connection impossible");
+// DB Connection
+mysql_select_db($db) or die("pas de connection");
+?>
 

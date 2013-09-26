5d7aca19272b15cf36d6769297a9f226c4ce03b8
Yasser Rabi <yachironi@hotmail.com>
2013-09-26 08:55:11 +0100

    fix

--- config.php
+++ config.php
@@ -1,12 +1,7 @@
 <?php 
-  
-<<<<<<< HEAD
+
 $db="yasser_cloud";   
 $connection = mysql_connect("host","user","password") or die ("Database Connection Problem"); 
-=======
-$db="DB Name";   
-$connection = mysql_connect("host","user","password") or die ("Get the f****k out!"); 
->>>>>>> 60ceefbae4c5f9071161290870a96810fa763079
 // test la connection 
 if ( ! $connection ) 
   die ("connection impossible"); 

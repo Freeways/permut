370879f5f62550df33f9bd783e939b0137646770
Yasser Rabi <yachironi@hotmail.com>
2013-09-29 13:18:29 +0100

    delete test parameters

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$db = "permut";
-$connection = mysql_connect("localhost", "test", "test") or die("Database Connection Problem");
+$db = "db";
+$connection = mysql_connect("host", "user", "password") or die("Database Connection Problem");
 // Test Connection
 if (!$connection)
 	die("connection impossible");
